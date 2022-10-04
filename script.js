10ac82c2e5dbf7b0e0e428653827a0a9acde209f
unknown <pradogeovana07@gmail.com>
2022-10-03 22:38:28 -0700

    finished project

--- script.js
+++ script.js
@@ -1,44 +1,54 @@
-function getRandomLower(){
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-    
-  }
-  
-  function getRandomUpper(){
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-   
-  }
-  
-  function getRandomNumber(){
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-  
-  }
-  
-  function getRandomSymbol(){
-    return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
-  }
-  
-  
-  // Assignment Code
-  var generateBtn = document.querySelector("#generate");
-  
-  
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  }
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
-  
-  function generatePassword() {
+var generateBtn = document.querySelector("#generate");
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+function generatePassword() {
     var passwordCharSet = "";
-  
-    for (let i = 0; i < length; i++) {
-      password += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
+
+    var passwordLength = prompt("Password Length? \(must be 8-128 characters long\)")
+
+    var lower = confirm("Lowercase?")
+    var upper = confirm("Uppercase?")
+    var number = confirm("Numbers?")
+    var symbol = confirm("Symbols?")
+
+    var lowercase = "abcdefghijklmnopqrstuvwxyz"
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var numbers = "1234567890"
+    var symbols = "!@#$%^&*()_+="
+
+    if(lower){
+        passwordCharSet += lowercase
     }
-    return password;
+
+    if(upper){
+        passwordCharSet += uppercase
+     }
+    if(numbers){
+        passwordCharSet += numbers
+    }
+
+    if(symbols){
+        passwordCharSet += symbols
+    }
+
+
+  var generatedPassword = "";
+
+  for (let i = 0; i < passwordLength; i++) {
+    generatedPassword += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
   }
-  
+  return generatedPassword;
+}
